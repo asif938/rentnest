b44e9be3b44796b00af7d44562c435a261d68d74
asif938 <asif1962000@gmail.com>
2026-07-08 06:13:49 +0600

    project initialized and models are created

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
+
+
+
+
+const app : Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+
+// app.use("/api/subscription/webhook", express.raw({ type: 'application/json' }))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
+
+
+// app.use("/api/users", userRoutes)
+// app.use("/api/auth", authRoutes)
+// app.use("/api/posts", postRoutes)
+// app.use("/api/comments", commentRoutes)
+// app.use("/api/subscription", subscriptionRoutes)
+// app.use("/api/premium", premiumRoutes)
+
+app.use(notFound)
+
+app.use(globalErrorHandler)
+
+export default app;
